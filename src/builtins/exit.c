d8d70e6ecef288aebf4ce96975c7e8f292a38f12
rajh-phuyal <rajhphuyal44@gmail.com>
2023-11-11 15:42:15 +0000

    adding initial file templates

---
+++ src/builtins/exit.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rajphuyal <rajphuyal@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 19:58:52 by rajphuyal         #+#    #+#             */
+/*   Updated: 2023/11/11 14:15:55 by rajphuyal        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void    exit(t_minivault *minivault, int status)
+{
+    // free the minivault
+    // exit the program with the status
+}
---
+++ src/builtins/pwd.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rajphuyal <rajphuyal@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 19:59:25 by rajphuyal         #+#    #+#             */
+/*   Updated: 2023/11/11 14:16:06 by rajphuyal        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void    pwd(t_minivault *minivault)
+{
+    // print the current working directory
+    // printing to the stdout or to a file while redirecting
+}
